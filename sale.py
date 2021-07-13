687893604a948024f934eddb8d739fc64b50f9b6
Juan Andrade <jpas.juan23@gmail.com>
2021-07-13 16:45:34 -0500

    Complete sale.py file.

---
+++ sale.py
@@ -0,0 +1,7 @@
+class Sale:
+    def __init__(self, inv, dat, cli, tot, sale_det):
+        self.invoice = inv
+        self.date = dat
+        self.client = cli
+        self.total = tot
+        self.sale_detail = sale_det
