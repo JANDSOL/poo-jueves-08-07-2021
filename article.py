aec739027fd97b62cd73d92502f6ef7dc9ff0327
Juan Andrade <jpas.juan23@gmail.com>
2021-07-13 16:39:09 -0500

    File article.py complete.

---
+++ article.py
@@ -0,0 +1,13 @@
+"""
+Article
+Client
+Sale
+SaleDet
+"""
+
+class Article:
+    def __init__(self, cod, des, pri, sto):
+        self.code = cod
+        self.description = des
+        self.price = pri
+        self.stock = sto
